0dc372da52807ff4b982f1d2bcf9c5faebb29a26
Mark Gatere <91592218+gateremark@users.noreply.github.com>
2025-05-10 12:49:25 +0300

    Update discount.js

--- problems/easy/discount/discount.js
+++ problems/easy/discount/discount.js
@@ -1,6 +1,10 @@
 
 function getDiscount(p,d) {
         // write your code here
+        let finalP = p - (p * d / 100);
+        let roundP = finalP.toFixed(2);
+
+        return roundP;
 
     return 0;
 }
