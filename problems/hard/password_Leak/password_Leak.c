edfd0d57a831c915afba35cf49147c75b7492cd6
wmishie <160480192+wmishie@users.noreply.github.com>
2025-05-10 13:37:03 +0300

    Update password_Leak.c

--- problems/hard/password_Leak/password_Leak.c
+++ problems/hard/password_Leak/password_Leak.c
@@ -11,7 +11,7 @@ int is_anagram(const int* a, const int* b) {
     return 1;
 }
 
-int* find_anagram_indices(char* s, char* p, int* returnSize) {
+int* find_anagram_indices(const char* s, const char* p, int* returnSize) {
     int s_len = strlen(s);
     int p_len = strlen(p);
     int* result = (int*)malloc(MAX_RESULT * sizeof(int));
@@ -20,24 +20,39 @@ int* find_anagram_indices(char* s, char* p, int* returnSize) {
     if (s_len < p_len) return result;
 
     int p_count[26] = {0};
-    int s_count[26] = {0};
+    int window_count[26] = {0};
 
     for (int i = 0; i < p_len; i++) {
         p_count[p[i] - 'a']++;
-        s_count[s[i] - 'a']++;
+        window_count[s[i] - 'a']++;
     }
 
     for (int i = 0; i <= s_len - p_len; i++) {
         if (i > 0) {
-            // Slide the window: remove one, add one
-            s_count[s[i - 1] - 'a']--;
-            s_count[s[i + p_len - 1] - 'a']++;
+            window_count[s[i - 1] - 'a']--;
+            window_count[s[i + p_len - 1] - 'a']++;
         }
 
-        if (is_anagram(p_count, s_count)) {
+        if (is_anagram(p_count, window_count)) {
             result[(*returnSize)++] = i;
         }
     }
 
     return result;
 }
+
+int main() {
+    const char* s = "cbaebabacd";
+    const char* p = "abc";
+    int returnSize;
+    int* indices = find_anagram_indices(s, p, &returnSize);
+
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d ", indices[i]);
+    }
+    printf("\n");
+
+    free(indices);
+    return 0;
+}
+
