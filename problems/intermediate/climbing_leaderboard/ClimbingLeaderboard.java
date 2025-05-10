8dde8746fec52e1948b56e17a61bb086cb2a4f9a
gracengina <gracemwangi412@gmail.com>
2025-05-10 13:15:14 +0300

    Update ClimbingLeaderboard.java

--- problems/intermediate/climbing_leaderboard/ClimbingLeaderboard.java
+++ problems/intermediate/climbing_leaderboard/ClimbingLeaderboard.java
@@ -3,16 +3,29 @@ package problems.intermediate.climbing_leaderboard;
 import java.util.*;
 
 public class ClimbingLeaderboard {
-    /**
-     * Returns the player's rank after each game.
-     * @param ranked the leaderboard scores
-     * @param player the player's scores
-     * @return List of player ranks after each game
-     */
+     List<Integer> uniqueRanks = new ArrayList<>();
+        for (int score : ranked) {
+            int lastScore = -1;
+
+for (int score : ranked) {
+    if (score != lastScore) {
+        uniqueRanks.add(score);
+        lastScore = score;
+    }
+        }
     
     public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
         List<Integer> result = new ArrayList<>();
-        // Write your code here
+         List<Integer> result = new ArrayList<>();
+        int index = uniqueRanks.size() - 1;
+
+        for (int pScore : player) {
+           
+            while (index >= 0 && pScore >= uniqueRanks.get(index)) {
+                index--;
+            }
+            result.add(index + 2); 
+        }
         return result;
     }
-}
+}}
