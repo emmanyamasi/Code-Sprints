1bd08e3a48f6768a2b44ca614c28bd826f2a66c6
gracengina <gracemwangi412@gmail.com>
2025-05-10 13:27:43 +0300

    Update climbing_leaderboard.py

--- problems/intermediate/climbing_leaderboard/climbing_leaderboard.py
+++ problems/intermediate/climbing_leaderboard/climbing_leaderboard.py
@@ -1,4 +1,14 @@
 
 def climbingLeaderboard(ranked, player):
-    # Write your code here
-    return []
+   
+    unique_scores = sorted(set(ranked), reverse=True)
+    result = []
+    i = len(unique_scores) - 1
+
+    for score in player:
+        while i >= 0 and score >= unique_scores[i]:
+            i -= 1
+        result.append(i + 2)  
+
+    return result
+
